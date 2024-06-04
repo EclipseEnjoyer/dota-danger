c82f5167876fc563d78f0197d70563d8a79fb434
Dustin Avery <1998davery2020@gmail.com>
2024-06-03 23:56:48 -0500

    Adding some basic styling and transitions to somewhat match inspiration material

--- src/app/basic/page.tsx
+++ src/app/basic/page.tsx
@@ -1,31 +1,51 @@
-import Image from "next/image";
+'use client'
 
-function QuestionBox({ value } : { value: number }) {
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+
+function QuestionBox({ value, question } : { value: number, question: string }) {
+  const [isOpen, setOpen] = useState(false)
+  const [isAnswered, setAnswered] = useState(false)
+
   return (
-    <td>
-      { value }
-      <Image
-          className="relative"
-          src="/icons/Gold_symbol.webp"
-          alt="Gold Symbol"
-          width={18}
-          height={18}
-          priority
-        />
-    </td>
+    <motion.td layout className={ isOpen ? "block fixed top-0 left-0 h-screen w-screen bg-red-950 z-10" : "py-2 border-solid border-white border-2"}>
+      <motion.span layout className={isOpen ? "hidden" : "flex flex-row items-center justify-center"}  onClick={() => {if(!isAnswered) setOpen(!isOpen) }}>
+        <span className={isAnswered ? "hidden" : "bold"}>{ value }</span>
+        <Image
+            className={isAnswered ? "hidden" : "relative"}
+            src="/icons/Gold_symbol.webp"
+            alt="Gold Symbol"
+            width={18}
+            height={18}
+            priority
+          />
+      </motion.span>
+      <motion.div layout className={isOpen ? "h-screen w-screen flex flex-col items-center justify-center gap-6" : "hidden"} onKeyDown={(event) => {
+        console.log('Key event?', event)
+        if(event.code === "Enter")setOpen(!isOpen);setAnswered(true);
+        }}>
+        <div className="text-5xl font-extrabold">{ question }</div>
+        <div className="text-5xl font-light italic">What is
+        <input className="bg-transparent border-solid border-b-4 border-black ml-6"/>
+        ?</div>
+      </motion.div>
+    </motion.td>
   )
 }
 
 function QuestionRow({value, columns} : { value: number, columns: Array<string>}){
+  //Need to add in a way to load in questions
   const questionBoxes = columns.map(column =>
-    <QuestionBox key={column + value.toString()} value={value}/>
+    <QuestionBox  key={column + value.toString()} value={value} question={"Some random question"}/>
   );
   return (<tr>{questionBoxes}</tr>);
 }
 
 function QuestionTable({columns, rows, step} : {columns: Array<string>, rows: number, step: number}){
   const tableHeaders = columns.map(column =>
-    <th key={"header_" + column}>{column}</th>
+    <th className="px-2 py-2 min-w-28 border-solid border-white border-2" key={"header_" + column}>{column}</th>
   )
   //I feel like there has to be a better way to do this, in terms of code looking nice
   let rowArray = [];
@@ -36,7 +56,7 @@ function QuestionTable({columns, rows, step} : {columns: Array<string>, rows: nu
     <QuestionRow key={"row_" + rowValue} value={rowValue} columns={columns}/>
   )
   return (
-    <table >
+    <table className="bg-black">
       <thead>
         <tr>
           {tableHeaders}
