b04dfc33ad446ab31e320d4264702a695c2bee7c
Dustin Avery <1998davery2020@gmail.com>
2024-06-06 16:39:22 -0500

    Updating the layout page to better serve as a header for the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="z-10 w-full text-center">
-          <span className="font-serif text-5xl text-transparent bg-clip-text bg-gradient-to-b from-zinc-600 via-amber-300 to-amber-950">
-            Dota Danger
-          </span>
+        <div className="z-10 w-full h-10 text-center">
+        <Link key={'Home'} href={'/'}>
+            <span className="font-serif text-5xl text-transparent bg-clip-text bg-gradient-to-b from-zinc-600 via-amber-300 to-amber-950">
+              Dota Danger
+            </span>
+          </Link>
         </div>
         {children}
       </body>
