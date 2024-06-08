45e5bfe456b61f91026e45afb46575bbda8259fd
Dustin Avery <1998davery2020@gmail.com>
2024-06-08 15:15:21 -0500

    Getting a baseline better component for player information to prepare for the socket-ization of the application

--- src/app/solo/page.tsx
+++ src/app/solo/page.tsx
@@ -5,32 +5,16 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { act, useState } from "react";
 import QuestionTable from "@/components/QuestionTable";
+import PlayerInformationTracker from "@/components/PlayerInformationTracker";
 const boardInfo = require("../../../public/boardFiles/basicBoard1.json")
-
-function PlayerGoldTracker({ playerName, gold } : {playerName: string, gold: number}){
-  return (
-    <div className="bg-teal-900 w-60 flex flex-col text-center gap-4 border-solid border-black border-2">
-      <div className="flex flex-row gap-1 text-center items-center justify-center">
-          <Image
-            src="/icons/Gold_symbol.webp"
-            alt="Gold Symbol"
-            width={18}
-            height={18}
-            priority
-          />
-        <span className="text-4xl text-transparent bg-clip-text bg-gradient-to-b from-zinc-600 via-amber-300 to-amber-950">{ gold }</span>
-      </div>
-      <div className="text-4xl">{ playerName }</div>
-    </div>
-  )
-}
 
 export default function Page() {
   const [playerArray, setPlayerArray] = useState([{name: 'Player1', gold: 500}]);
   const [activePlayer, setActivePlayer] = useState(0)
+  const [clientPlayer, setClientPlayer] = useState(0)
 
   function setActivePlayerGold(index: number, goldValue: number){
-    let newPlayerArray = playerArray
+    let newPlayerArray = structuredClone(playerArray)
     newPlayerArray[activePlayer].gold = goldValue
     setPlayerArray(newPlayerArray)
   }
@@ -44,7 +28,7 @@ export default function Page() {
 
   //This shouldn't use active player in the long run but just for ease of implementation right now
   const setPlayerName = (name: string) => {
-    let newPlayerArray = playerArray
+    let newPlayerArray = structuredClone(playerArray)
     newPlayerArray[activePlayer].name = name
     setPlayerArray(newPlayerArray)
   }
@@ -55,7 +39,7 @@ export default function Page() {
         <QuestionTable key="question-table" boardInfo={boardInfo} goldAdjustmentFunction={adjustActivePlayerGold}/>
       </div>
       <div className="fixed bottom-0 z-20 flex text-center justify-center h-1/6 mx-5">
-        <PlayerGoldTracker key="player1-gold-tracker" playerName={playerArray[0].name} gold={playerArray[0].gold}/>
+        <PlayerInformationTracker key="player1-gold-tracker" playerArray={playerArray} nameAdjustmentFunction={setPlayerName}/>
       </div>
     </main>
   );
---
+++ src/components/PlayerInformationTracker.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+function PlayerGoldTracker({ playerName, gold, nameAdjustmentFunction } : {playerName: string, gold: number, nameAdjustmentFunction: (name: string) => void}){
+  let textSize = "text-4xl"
+
+  if(playerName.length > 9) textSize = "text-2xl"
+  if(playerName.length > 14) textSize = "text-lg"
+
+  return (
+    <div className="bg-teal-900 w-60 h-full flex flex-col text-center gap-4 border-solid border-black border-2">
+      <div className="flex flex-row gap-1 text-center items-center justify-center">
+          <Image
+            src="/icons/Gold_symbol.webp"
+            alt="Gold Symbol"
+            width={18}
+            height={18}
+            priority
+          />
+        <span className="text-4xl text-transparent bg-clip-text bg-gradient-to-b from-zinc-600 via-amber-300 to-amber-950">{ gold }</span>
+      </div>
+      <input className={"bg-transparent text-center m-6 mt-2 " + textSize } value={ playerName } onChange={e => nameAdjustmentFunction(e.target.value)}/>
+    </div>
+  )
+}
+export default function PlayerInformationTracker({ playerArray, nameAdjustmentFunction } : { playerArray: Array<any>, nameAdjustmentFunction: (name: string) => void }) {
+  const playerTrackers = playerArray.map((playerObject, index) => 
+    <PlayerGoldTracker key={"player_" + index} playerName={playerObject.name} gold={playerObject.gold} nameAdjustmentFunction={nameAdjustmentFunction}/>
+  )
+  return (
+    <div className="w-screen flex flex-col text-center gap-4 justify-center items-center">
+      {playerTrackers}
+    </div>
+  )
+}
